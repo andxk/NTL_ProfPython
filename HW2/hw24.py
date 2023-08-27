878642eac72d2a75c500145ef6a2c63279c7bc7d
andxk <kupex@ya.ru>
2023-08-27 23:20:35 +0300

    HW2.4

---
+++ HW2/hw24.py
@@ -0,0 +1,40 @@
+import types
+
+
+
+def flat_generator(list_of_list):
+    for s in list_of_list:
+        if isinstance(s, list):
+            for x in flat_generator(s):
+                yield x
+        else:
+            yield s
+
+
+
+
+def test_4():
+
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+##    for x in flat_generator(list_of_lists_2):
+##        print(x)
+
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+    assert isinstance(flat_generator(list_of_lists_2), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_4()
